45611833db00bb774a64c7c6f571091ad3141e94
SaulRuizNegrete <saul53141@gmail.com>
2023-10-02 22:10:01 -0600

    chore(nextui): add additional tailwind config with vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
+
 // tailwind.config.js
-const {nextui} = require("@nextui-org/react");
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    // ...
-    "./src/**/*.{html,js}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [nextui()],
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
 };
+export const darkMode = "class";
+export const plugins = [nextui()];
